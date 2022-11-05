ee48a24455a21557ef4945f736a43998df4dd2cc
@Draichi <lucasdraichi@gmail.com>
2022-11-04 22:58:33 -0500

    feat: expanded mobile menu

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,2 @@
 import "./style.css";
-
-const menuButton =
-  document.querySelector<HTMLButtonElement>("button.menu-icon")!;
-
-const closeMenuButton = document.querySelector<HTMLButtonElement>(
-  "button.close-menu-icon"
-)!;
-
-const mobileMenuOverlay =
-  document.querySelector<HTMLDivElement>(".menu-mobile")!;
-
-menuButton.addEventListener("click", () => {
-  mobileMenuOverlay.classList.add("is-visible");
-  mobileMenuOverlay.classList.remove("is-hidden");
-
-  closeMenuButton.addEventListener("click", () => {
-    mobileMenuOverlay.classList.remove("is-visible");
-    mobileMenuOverlay.classList.add("is-hidden");
-  });
-});
+import "./app/navbar";
