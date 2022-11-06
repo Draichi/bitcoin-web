f277d5f67830263cb5b9027c1a23df48c2f77272
@Draichi <lucasdraichi@gmail.com>
2022-11-06 14:24:38 -0500

    chore: add and configure eslint

--- src/app/navbar.ts
+++ src/app/navbar.ts
@@ -1,31 +1,31 @@
 const menuButton = document.querySelector<HTMLButtonElement>(
-  ".navbar__hamburguer-icon"
-)!;
+  '.navbar__hamburguer-icon'
+)!
 
 const closeMenuButton = document.querySelector<HTMLButtonElement>(
-  ".navbar__times-icon"
-)!;
+  '.navbar__times-icon'
+)!
 
 const menuItems = document.querySelectorAll<HTMLAnchorElement>(
-  ".expanded-mobile-menu__item"
-)!;
+  '.expanded-mobile-menu__item'
+)!
 
 const mobileMenuOverlay = document.querySelector<HTMLDivElement>(
-  ".expanded-mobile-menu"
-)!;
+  '.expanded-mobile-menu'
+)!
 
-menuButton.addEventListener("click", () => {
-  mobileMenuOverlay.style.display = "flex";
-});
+menuButton.addEventListener('click', () => {
+  mobileMenuOverlay.style.display = 'flex'
+})
 
-closeMenuButton.addEventListener("click", () => {
-  mobileMenuOverlay.style.display = "none";
-});
+closeMenuButton.addEventListener('click', () => {
+  mobileMenuOverlay.style.display = 'none'
+})
 
 menuItems.forEach((menuItem) => {
-  menuItem.addEventListener("click", () => {
-    mobileMenuOverlay.style.display = "none";
-  });
-});
+  menuItem.addEventListener('click', () => {
+    mobileMenuOverlay.style.display = 'none'
+  })
+})
 
-export {};
+export {}
